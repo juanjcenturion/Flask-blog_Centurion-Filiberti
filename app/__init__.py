7127028146b891495ce3581b6febc573233abda3
Juan Jose Centurion <j.centurion@itecriocuarto.org.ar>
2023-10-24 10:03:10 -0300

    test

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 import os
 #IMPORTS NATIVOS DEL FRAMEWORK.
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
